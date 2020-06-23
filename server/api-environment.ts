440e56d2010ac767fd9d338de786d64e0983907e
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2020-06-23 16:39:12 +0100

    Add api environment file

---
+++ server/api-environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  appId: 'Alexande-BayBarga-SBX-1c8e8a00c-d31e69a0'
+  // browseBaseUrl: 'https://api.sandbox.ebay.com/buy/browse/v1',
+  // findBaseUrl: 'https://svcs.sandbox.ebay.com/services/search/FindingService/v1'
+};
+
